e75eb77a98b925cbca2a4792f6c20f1d3849dabe
AMM48 <abdulrahmanalmehdar48@gmail.com>
2023-05-16 18:31:43 +0300

    .

--- src/App.js
+++ src/App.js
@@ -11,24 +11,26 @@ function App() {
 
   return (
     <div className="App flex">
-      <div id="search">
-        <input
-          type="search"
-          value={data.name}
-          onChange={handleChange}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              fetchData();
-            }
-          }}
-          placeholder="Enter City Name"
-        />
-        <button onClick={fetchData}>
-          <BsSearch /> Search
-        </button>
+      <div className="container">
+        <div id="search">
+          <input
+            type="search"
+            value={data.name}
+            onChange={handleChange}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                fetchData();
+              }
+            }}
+            placeholder="Enter City Name"
+          />
+          <button onClick={fetchData}>
+            <BsSearch />
+          </button>
+        </div>
+        <CurrentWeather {...tempUnitObj} />
+        <Forecast {...tempUnitObj} />
       </div>
-      <CurrentWeather {...tempUnitObj} />
-      <Forecast {...tempUnitObj} />
     </div>
   );
 }
