574f189fde1b861eeb535f9de89263e6e7e0e508
AMM48 <abdulrahmanalmehdar48@gmail.com>
2023-05-17 19:04:06 +0300

    .

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import About from "./components/About.jsx";
-import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Switch, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
-    <Routes>
+    <Switch>
       <Route path="/fluffy-journey" element={<App />} />
       <Route path="/fluffy-journey/about" element={<About />} />
-    </Routes>
+    </Switch>
   </HashRouter>
 );
